d86fa35869391bbd07fe03dbfb3a05efda13b644
matifryz <mateuszfryziel.mf@gmail.com>
2019-01-27 21:45:02 +0100

    dodanie kontrolera kus i nauczyciel

---
+++ Szkola/Controllers/KursController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Szkola.DBAcess;
+using Szkola.Models;
+
+namespace Szkola.Controllers
+{
+    public class KursController : Controller
+    {
+        private SzkolaContext db = new SzkolaContext();
+
+        // GET: Kurs
+        public ActionResult Index()
+        {
+            return View(db.Kursy.ToList());
+        }
+
+        // GET: Kurs/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kurs kurs = db.Kursy.Find(id);
+            if (kurs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kurs);
+        }
+
+        // GET: Kurs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kurs/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdKurs,Nazwa,LStudentów")] Kurs kurs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kursy.Add(kurs);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kurs);
+        }
+
+        // GET: Kurs/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kurs kurs = db.Kursy.Find(id);
+            if (kurs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kurs);
+        }
+
+        // POST: Kurs/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdKurs,Nazwa,LStudentów")] Kurs kurs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kurs).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kurs);
+        }
+
+        // GET: Kurs/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kurs kurs = db.Kursy.Find(id);
+            if (kurs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kurs);
+        }
+
+        // POST: Kurs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Kurs kurs = db.Kursy.Find(id);
+            db.Kursy.Remove(kurs);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
